257df0510162bdcdf7890092c58e0af7d557590e
Tejender Sheoran <tsheoran@cisco.com>
2017-10-03 13:13:05 +0530

    Update

--- bash_setup.sh
+++ bash_setup.sh
@@ -5,7 +5,7 @@ set -x
 cd ~
 rm .bash_profile
 ln -s ~/tbash/.bash_profile .bash_profile
-echo "source ~/.bashrc" >> ~/.bashrc
+echo "alias sb='source ~/.bash_profile'" >> ~/.bashrc
 set +x
 echo "bash setup complete, update .bash_profile.local and then type bash"
 
