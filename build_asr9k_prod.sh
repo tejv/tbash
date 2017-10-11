8eed5ece9d294405b066bd30b66d49ea47c0a50d
Tejender Sheoran <tejendersheoran@gmail.com>
2017-10-11 21:52:59 +0530

    Update build_asr9k_prod.sh

--- build_asr9k_prod.sh
+++ build_asr9k_prod.sh
@@ -4,8 +4,8 @@ echo "Build started"
 set -x
 cd /nobackup/tsheoran/dev/"$1"/
 jam -l -d0 -j64 asr9k-mini-x64.iso
-echo "Running static analysis"
 /auto/ses/bin/run_sa
+ctags -R
 set +x
 echo "Build over"
 
