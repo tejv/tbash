8c2b214bdf2b969349f415027895c7c1ab6dc8dd
Tejender Sheoran <tejendersheoran@gmail.com>
2017-10-12 13:31:05 +0530

    Update build_asr9k_prod.sh

--- build_asr9k_prod.sh
+++ build_asr9k_prod.sh
@@ -3,9 +3,9 @@
 echo "Build started"
 set -x
 cd /nobackup/tsheoran/dev/"$1"/
-jam -l -d0 -j64 asr9k-mini-x64.iso
-/auto/ses/bin/run_sa
-ctags -R
+jam -l -d0 -j64 asr9k-mini-x64.iso &&
+/auto/ses/bin/run_sa  &&
+ctags -R &&
 set +x
 echo "Build over"
 
