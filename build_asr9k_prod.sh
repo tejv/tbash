cddc73589bda2e75d580cca72be8bb30fb515736
Tejender Sheoran <tejendersheoran@gmail.com>
2017-10-16 21:31:40 +0530

    Update build_asr9k_prod.sh

--- build_asr9k_prod.sh
+++ build_asr9k_prod.sh
@@ -6,6 +6,8 @@ cd /nobackup/tsheoran/dev/"$1"/
 jam -l -d0 -j64 asr9k-mini-x64.iso > /dev/null
 /auto/ses/bin/run_sa
 ctags -R  * >& /dev/null
+cd calvados/
+ctags -R * >& /dev/null
 set +x
 echo "Build over"
 
