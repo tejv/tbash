eccf101e73994acfa2a855389793c9cc41223627
Tejender Sheoran <tejendersheoran@gmail.com>
2017-10-17 16:38:55 +0530

    Update build_n_load_tftp_asr9k_prod.sh

--- build_n_load_tftp_asr9k_prod.sh
+++ build_n_load_tftp_asr9k_prod.sh
@@ -5,6 +5,7 @@ set -x
 cd /nobackup/tsheoran/dev/"$1"/
 jam -l -d0 -j64 asr9k-mini-x64.iso > /dev/null
 cp /nobackup/tsheoran/dev/"$1"/img-asr9k/asr9k-mini-x64.iso /auto/tftp-blr-users4/tsheoran/.
+cp /nobackup/tsheoran/dev/"$1"/img-asr9k/asr9k-mini-x64.iso ~/dev/temp/.
 /auto/ses/bin/run_sa
 ctags -R * >& /dev/null
 cd calvados/
