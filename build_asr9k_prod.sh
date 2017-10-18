3c94ed86ca7f576d3b5e391b0563eed1be086062
Tejender Sheoran <tejendersheoran@gmail.com>
2017-10-18 13:58:59 +0530

    Update build_asr9k_prod.sh

--- build_asr9k_prod.sh
+++ build_asr9k_prod.sh
@@ -3,15 +3,9 @@
 echo "Build started"
 set -x
 cd /nobackup/tsheoran/dev/"$1"/
-jam -l -d0 -j64 asr9k-mini-x64.iso > /dev/null
-cp /nobackup/tsheoran/dev/"$1"/img-asr9k/asr9k-mini-x64.iso ~/dev/temp/.
-rm -rf ~/dev/temp/ctrace_dec/
-mkdir ~/dev/temp/ctrace_dec/
-cp -R /nobackup/tsheoran/dev/"$1"/ctrace_dec/  ~/dev/temp/
-/auto/ses/bin/run_sa
-ctags -R  * >& /dev/null
-cd calvados/
-ctags -R * >& /dev/null
+jam -l -d0 -j64 asr9k-mini-x64.iso
 set +x
 echo "Build over"
+
+sh ~/teju/bash/build_post_process.sh $1
 
