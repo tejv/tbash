65d9f9b0c0ecb6be141d175e4fd4780feefca8f3
Tejender Sheoran <tejendersheoran@gmail.com>
2017-10-18 14:23:35 +0530

    Update build_post_process.sh

--- build_post_process.sh
+++ build_post_process.sh
@@ -2,12 +2,13 @@
 
 echo "Build post process"
 set -x
+NEW_DIR='~/dev/temp/$(date -d "today" +"%M%H%d%m%Y")_bk/'
+mkdir $NEW_DIR
 cd /nobackup/tsheoran/dev/"$1"/
-cp /nobackup/tsheoran/dev/"$1"/img-asr9k/asr9k-mini-x64.iso ~/dev/temp/.
-rm -rf ~/dev/temp/ctrace_dec/
-mkdir ~/dev/temp/ctrace_dec/
-cp -R /nobackup/tsheoran/dev/"$1"/ctrace_dec/  ~/dev/temp/
-/auto/ses/bin/run_sa
+cp /nobackup/tsheoran/dev/"$1"/img-asr9k/asr9k-mini-x64.iso $NEW_DIR/
+cp -R /nobackup/tsheoran/dev/"$1"/ctrace_dec/  $NEW_DIR/
+acme diff > $NEW_DIR/diff
+/auto/ses/bin/run_sa > $NEW_DIR/sa
 ctags -R * >& /dev/null
 cd calvados/
 ctags -R * >& /dev/null
