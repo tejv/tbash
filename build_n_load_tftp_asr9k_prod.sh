f27d70a5bf13d6bf8d2151ad104185c622fd8fae
Tejender Sheoran <tejendersheoran@gmail.com>
2017-10-18 13:55:56 +0530

    Update build_n_load_tftp_asr9k_prod.sh

--- build_n_load_tftp_asr9k_prod.sh
+++ build_n_load_tftp_asr9k_prod.sh
@@ -5,13 +5,7 @@ set -x
 cd /nobackup/tsheoran/dev/"$1"/
 jam -l -d0 -j64 asr9k-mini-x64.iso > /dev/null
 cp /nobackup/tsheoran/dev/"$1"/img-asr9k/asr9k-mini-x64.iso /auto/tftp-blr-users4/tsheoran/.
-cp /nobackup/tsheoran/dev/"$1"/img-asr9k/asr9k-mini-x64.iso ~/dev/temp/.
-rm -rf ~/dev/temp/ctrace_dec/
-mkdir ~/dev/temp/ctrace_dec/
-cp -R /nobackup/tsheoran/dev/"$1"/ctrace_dec/  ~/dev/temp/
-/auto/ses/bin/run_sa
-ctags -R * >& /dev/null
-cd calvados/
-ctags -R * >& /dev/null
 set +x
 echo "Build and load over"
+
+sh ~/teju/bash/build_post_process.sh $1
