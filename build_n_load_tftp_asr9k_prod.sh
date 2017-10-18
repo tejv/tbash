e881b4e1e4561ca11cd3e475968f77b57e72f894
Tejender Sheoran <tejendersheoran@gmail.com>
2017-10-18 13:59:12 +0530

    Update build_n_load_tftp_asr9k_prod.sh

--- build_n_load_tftp_asr9k_prod.sh
+++ build_n_load_tftp_asr9k_prod.sh
@@ -3,7 +3,7 @@
 echo "Build and load started"
 set -x
 cd /nobackup/tsheoran/dev/"$1"/
-jam -l -d0 -j64 asr9k-mini-x64.iso > /dev/null
+jam -l -d0 -j64 asr9k-mini-x64.iso
 cp /nobackup/tsheoran/dev/"$1"/img-asr9k/asr9k-mini-x64.iso /auto/tftp-blr-users4/tsheoran/.
 set +x
 echo "Build and load over"
