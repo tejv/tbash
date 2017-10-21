16139c7d920e9a4cd31ff145228fb2653cae2e81
Tejender Sheoran <tejendersheoran@gmail.com>
2017-10-21 16:39:00 +0530

    Update build_post_process.sh

--- build_post_process.sh
+++ build_post_process.sh
@@ -8,7 +8,7 @@ if [ ! -d "$BASE_DIR" ]; then
   mkdir $BASE_DIR
 fi
 
-NEW_DIR='~/dev/temp/$1/$(date -d "today" +"%M%H%d%m%Y")_bk/'
+NEW_DIR=~/dev/temp/$1/$(date -d "today" +"%M%H%d%m%Y")_bk/
 mkdir $NEW_DIR
 cd /nobackup/tsheoran/dev/"$1"/
 cp /nobackup/tsheoran/dev/"$1"/img-asr9k/asr9k-mini-x64.iso $NEW_DIR/
