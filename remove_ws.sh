3d6c559adc2ada0df9348d0206b1c3f4516cc585
Tejender Sheoran <tejendersheoran@gmail.com>
2017-10-24 07:35:59 +0530

    Update remove_ws.sh

--- remove_ws.sh
+++ remove_ws.sh
@@ -2,7 +2,7 @@
 
 WS_DIR=/nobackup/tsheoran/dev/$1
 TMP_DIR=/nobackup/tsheoran/dev/temp/$1
-echo "Removing WS $WS and $TMPDIR permanently. Are you sure[y/n]: "
+echo "Removing WS $WS_DIR and $TMP_DIR permanently. Are you sure[y/n]: "
 read userinput
 if [ "$userinput" == "y" ]; then
   set -x
