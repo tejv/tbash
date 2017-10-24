d9e18e1167e511ad244a7caa799bee276d4e4af2
Tejender Sheoran <tejendersheoran@gmail.com>
2017-10-24 16:27:46 +0530

    Update boot_asr9k.sh

--- boot_asr9k.sh
+++ boot_asr9k.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# -d devname, default='brahma', help='Device name')
+# -t testbed, default='/users/tsheoran/teju/tpython/asr9k_testbed.yaml', help='Testbed yaml file')
+# -n imagename, default='teju_default', help='Default boot image')
+# -i imageinfo, default='/users/tsheoran/teju/tpython/image_info.yaml'
+
 echo "Boot image started"
 set -x
 /router/bin/python ~/tpython/clear_line.py "$@"
