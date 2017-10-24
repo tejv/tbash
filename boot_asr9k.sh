cd2527103d0e49c0dc65e26d4e5af5671a396833
Tejender Sheoran <tsheoran@cisco.com>
2017-10-24 18:09:09 +0530

    update

--- boot_asr9k.sh
+++ boot_asr9k.sh
@@ -9,5 +9,6 @@ echo "Boot image started"
 set -x
 /router/bin/python ~/tpython/clear_line.py "$@"
 /router/bin/python ~/tpython/boot_asr9k.py "$@"
+/router/bin/python ~/tpython/conf_mgmt_asr9k.py "$@"
 set +x
 echo "Boot image over"
