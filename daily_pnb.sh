af8b545c424e5efcaac12466cfa7ef60eb972d33
Tejender Sheoran <tejendersheoran@gmail.com>
2017-10-25 12:01:19 +0530

    Update daily_pnb.sh

--- daily_pnb.sh
+++ daily_pnb.sh
@@ -3,6 +3,6 @@
 source ~/.bash_profile
 echo "Daily run"
 set -x
-sh ~/tbash/pull_n_build_asr9k.sh $(date -d "today" +"%d%m%Y")_ws
+sh ~/tbash/a9k_pull_n_build.sh $(date -d "today" +"%d%m%Y")_ws
 set +x
-echo "Pull and Build over"
+echo "Daily run over"
