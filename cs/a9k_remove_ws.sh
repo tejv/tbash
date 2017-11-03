66942d97d9f4bed7faa030dc1b096618b968c923
Tejender Sheoran <tejendersheoran@gmail.com>
2017-11-03 18:23:00 +0530

    Update a9k_remove_ws.sh

--- cs/a9k_remove_ws.sh
+++ cs/a9k_remove_ws.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
 
-WS_DIR=/nobackup/tsheoran/dev/$1
-TMP_DIR=/nobackup/tsheoran/dev/temp/$1
-echo "Removing WS $WS_DIR and $TMP_DIR permanently. Are you sure[y/n]: "
+if [ "$#" -eq 0 ]; then
+  echo "Usage: Provide ws name" >&2
+  exit 1
+fi
+
+echo "Removing following WS and TMP dirs permanently->"
+for var in "$@"
+do
+    WS_DIR=/nobackup/tsheoran/dev/$var
+    TMP_DIR=/nobackup/tsheoran/dev/temp/$var
+    echo "$WS_DIR, $TMP_DIR"
+done
+
+echo "Are you sure[y/n]: "
 read userinput
+
 if [ "$userinput" == "y" ]; then
-  set -x
-  rm -rf $WS_DIR
-  rm -rf $TMP_DIR
-  set +x
+  for var in "$@"
+  do
+      WS_DIR=/nobackup/tsheoran/dev/$var
+      TMP_DIR=/nobackup/tsheoran/dev/temp/$var
+      set -x
+      rm -rf $WS_DIR
+      rm -rf $TMP_DIR
+      set +x
+  done
   echo "WS Cleanup over"
   exit 0
 fi
