54ff4cde02579560d5a016321b2d4a77d91e7d41
Tejender Sheoran <tejendersheoran@gmail.com>
2017-11-12 17:14:34 +0530

    Update vim_setup.sh

--- vim_setup.sh
+++ vim_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo "Are you sure curl, git, vim, ctags, cscope and spf-13 are installed(y/n):"
+echo " Follow info repo to install required softwares first"
+echo "Are you sure curl, git, vim, tmux, reattach-to-user-namespace, cscope and spf-13 are installed(y/n):"
 read input
 if [ $input == "y" ]; then
     set -x
@@ -12,6 +13,12 @@ if [ $input == "y" ]; then
     ln -s ~/vim/lean/.vimrc.local .vimrc.local
     ln -s ~/vim/lean/.vimrc.before.local .vimrc.before.local
     ln -s ~/vim/lean/.vimrc.bundles.local .vimrc.bundles.local
+    
+    git clone https://github.com/gpakosz/.tmux.git
+    ln -s -f .tmux/.tmux.conf
+    rm .tmux.conf.local
+    ln -s ~/vim/lean/.tmux.conf.local .tmux.conf.local
+    
     set +x
     echo "Vim setup complete"
 else
