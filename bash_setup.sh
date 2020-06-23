8c70b1e082481724df4931a223cbe16afdba35c6
Tejender Sheoran <tejendersheoran@gmail.com>
2020-06-23 10:55:48 +0530

    Update bash_setup.sh

--- bash_setup.sh
+++ bash_setup.sh
@@ -3,6 +3,8 @@
 echo "Setting up bash"
 set -x
 cd ~
+cp .bash_profile .bash_profile_old
+cp .bashrc .bashrc_old
 rm .bash_profile
 ln -s ~/tbash/.bash_profile .bash_profile
 echo "alias sb='source ~/.bash_profile'" >> ~/.bashrc
